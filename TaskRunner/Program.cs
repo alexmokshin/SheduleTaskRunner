aeb95ccdbaf4793769c8899d702b574680531c9f
Мокшин Александр Вячеславович <mokshin.alex@gmail.com>
2019-04-12 21:58:40 +0500

    Add project files.

---
+++ TaskRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TaskRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
