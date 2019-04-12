4569359bd8712e5ac08a5f74b72b60ad98f70361
Мокшин Александр Вячеславович <mokshin.alex@gmail.com>
2019-04-12 23:17:53 +0500

    start project

--- TaskRunner/Program.cs
+++ TaskRunner/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.IO;
+using System.Xml;
 
 namespace TaskRunner
 {
@@ -8,6 +9,44 @@ namespace TaskRunner
     {
         static void Main(string[] args)
         {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(@"resources\config_list.xml");
+            //xDoc.Load(@"resources\config_list.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
+            {
+                ApplicationServiceBase application = new ApplicationServiceBase();
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("name");
+                    if (attr != null)
+                        application.Name = attr.Value;
+
+                }
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "HostAppName")
+                        application.HostApplicationName = childnode.InnerText;
+
+                    if (childnode.Name == "HostAddress")
+                        application.HostAddress = childnode.InnerText;
+
+                    if (childnode.Name == "IsManipulate")
+                        application.IsManipulate = Convert.ToBoolean(childnode.InnerText);
+
+                    if (childnode.Name == "Username")
+                        application.Username = childnode.InnerText;
+
+                    if (childnode.Name == "Password")
+                        application.Password = childnode.InnerText;
+
+                    if (childnode.Name == "CommandPrefix")
+                        application.CommandPrefix = childnode.InnerText;
+                    if (childnode.Name == "OS")
+                        application.OS = childnode.InnerText;
+                }
+                
+            }
 
             Console.WriteLine("Hello World!");
         }
---
+++ TaskRunner/SheduleTask.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using Oracle.ManagedDataAccess.Client;
+using System.IO;
+
+namespace TaskRunner
+{
+    class SheduleTask
+    {
+        public String Name { get; set; }
+        public DateTime Start_time { get; set; }
+        public String Sql_File_name { get; set; }
+        public String Frequency_type { get; set; }
+        public int Frequency { get; set; }
+        public String[] E_mails { get; set; }
+        #region Private variables
+        private OracleCommand command = null;
+        private String Sql_File_Command { get; set; }
+        private String FilePath { get; set; }
+        #endregion Private variables
+
+        
+
+        public SheduleTask(string Name, DateTime Start_time, string Sql_File_name, string Frequency_type, int Frequency, string[] emails)
+        {
+            this.Name = Name;
+            this.Start_time = Start_time;
+            this.Sql_File_name = Sql_File_name;
+            this.Frequency_type = Frequency_type;
+            this.Frequency = Frequency;
+            this.E_mails = emails;
+            FilePath = GetPathSqlFile();
+            if (File.Exists(FilePath))
+            {
+                using (StreamReader reader = new StreamReader(GetPathSqlFile()))
+                {
+                    Sql_File_Command = reader.ReadToEnd();
+                }
+            }
+            else
+                throw new Exception("Файл не существует в директории Tasks");
+            
+            
+        }
+
+        public async Task<int> SheduleTaskRunner(OracleConnection connection)
+        {
+            if (connection != null || !String.IsNullOrEmpty(Sql_File_Command))
+            {
+                command = new OracleCommand(Sql_File_Command, connection);
+            }
+            int resultCode = await command.ExecuteNonQueryAsync();
+
+            return resultCode;
+        }
+        
+        private String GetPathSqlFile()
+        {
+            string path = String.Empty;
+            if (!String.IsNullOrEmpty(Sql_File_name))
+            {
+                return $"Tasks/{Sql_File_name}";
+            }
+            else
+                throw new Exception("Имя файла не указано. Не удается найти путь");
+                
+        }
+
+
+    }
+}
